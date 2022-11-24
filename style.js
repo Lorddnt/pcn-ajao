07fc1afab6c205a1a1c5894c9da1c7a628856afc
Lorddnt <david.awoke@gmail.com>
2022-11-24 01:43:10 +0100

    First file

---
+++ style.js
@@ -0,0 +1,6 @@
+let burger = document.querySelector('.burger');
+    burger.onclick = function () {
+        let list = document.querySelector('.list');
+        list.classList.toggle('cue');
+        console.log("kdkdk")
+    }
